86dc9ceb3c0813e04a7fed9363f5097dbd298247
Daniel Lopez <dlopez@NSL-DLOPEZs-MacBook-Pro.local>
2020-03-24 18:15:19 -0700

    feature/compilador_de_operadores_de_comparacion
    - Permite compilar codigo fuente que incluye solo operadores de comparacion.

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import json
 from flask import Flask, render_template, request, jsonify
 from compilador.lexico import Lexico
 
@@ -8,6 +9,26 @@ def tabla_de_simbolos(*args, **kwargs):
     lexico = Lexico()
     simbolos = [{'token': s.token, 'lexema': s.lexema} for s in lexico.tabla_de_simbolos]
     return ({'simbolos': simbolos}, 200)
+
+@app.route('/compila/', methods=('post',))
+def compila(*args, **kwargs):
+    json_data = json.loads(request.data)
+    lexico = Lexico(codigo=json_data.get('codigo', ''))
+    componentes_lexicos = []
+    while True:
+        componente_lexico = lexico.siguiente_componente_lexico()
+        if componente_lexico:
+            componentes_lexicos.append(
+                {
+                    "token": componente_lexico.token,
+                    "lexema": componente_lexico.lexema,
+                }
+            )
+
+        else:
+            break
+
+    return ({'componentes_lexicos': componentes_lexicos}, 200)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- compilador/lexico.py
+++ compilador/lexico.py
@@ -7,7 +7,9 @@ PALABRAS_RESERVADAS = ('bool', 'call', 'char', 'do', 'else', 'float', 'for',
     'function', 'if', 'int', 'main', 'read', 'return', 'string', 'then', 'to',
     'void', 'while', 'write', 'false', 'true',)
 
-TOKENS = {constante: token for (token, constante) in enumerate(CONSTANTES) }
+TOKENS = {constante: token for (token, constante) in enumerate(CONSTANTES, 256)}
+
+SIMBOLOS_PERMITIDOS = r"(){}[],;+-*/\%&|!"
 
 class Simbolo(object):
     def __init__(self, token=None, lexema=None):
@@ -22,7 +24,8 @@ class Lexico(object):
     def __init__(self, codigo=""):
         self.codigo = codigo + " "
         self.tabla_de_simbolos = []
-        self.indice = 0
+        self.indice = -1
+        self.inicio_lexema = 0
         self.numero_de_linea = 1
         self.inicio = 0
         self.estado = 0
@@ -45,6 +48,30 @@ class Lexico(object):
         else:
             raise Exception("Debe proveer un Simbolo, o bien token y lexema!")
 
+    def __buscar_simbolo(self, lexema=''):
+        """
+        Recibe un lexema y busca un simbolo que coincida con el lexema en la
+        tabla de simbolos.
+        """
+        return next((s for s in self.tabla_de_simbolos if s.lexema == lexema), None)
+
+    def __siguiente_caracter(self):
+        """
+        Regresa el siguiente caracter en el codigo fuente.
+        """
+        try:
+            self.indice += 1
+            return self.codigo[self.indice]
+
+        except IndexError:
+            return None
+
+    def __avanza_inicio_lexema(self):
+        """
+        Mueve el inicio del lexema una posicion hacia adelante.
+        """
+        self.inicio_lexema = self.indice + 1
+
     def __cargar_palabras_reservadas(self):
         """
         Carga las palabras reservadas en la tabla de simbolos antes de iniciar
@@ -57,4 +84,106 @@ class Lexico(object):
                 ),
             PALABRAS_RESERVADAS)
         )
-            
+
+    def siguiente_componente_lexico(self):
+        """
+        Regresa el siguiente componente lexico (Simbolo) encontrado en el codigo
+        fuente.
+        """
+        while True:
+            if self.estado == 0:
+                caracter = self.__siguiente_caracter()
+                if caracter in (' ', '\t', '\n'):
+                    self.__avanza_inicio_lexema()
+                    if caracter == '\n':
+                        self.numero_de_linea += 1
+
+                elif caracter is None:
+                    return None
+
+                elif caracter == '<':
+                    self.estado = 1
+
+                elif caracter == '=':
+                    self.estado = 5
+
+                elif caracter == '>':
+                    self.estado = 6
+
+                else:
+                    self.estado = self.__fallo(self.inicio)
+
+            elif self.estado == 1:
+                caracter = self.__siguiente_caracter()
+                if caracter == '=':
+                    self.estado = 2
+
+                elif caracter == '>':
+                    self.estado = 3
+
+                else:
+                    self.estado = 4
+
+            elif self.estado == 2:
+                return Simbolo(token=TOKENS['MEI'], lexema=self.__leer_lexema())
+
+            elif self.estado == 3:
+                return Simbolo(token=TOKENS['DIF'], lexema=self.__leer_lexema())
+
+            elif self.estado == 4:
+                self.__retrocede_indice()
+                return Simbolo(token=TOKENS['MEN'], lexema=self.__leer_lexema())
+
+            elif self.estado == 5:
+                return Simbolo(token=TOKENS['IGU'], lexema=self.__leer_lexema())
+
+            elif self.estado == 6:
+                caracter = self.__siguiente_caracter()
+                if caracter == '=':
+                    self.estado = 7
+
+                else:
+                    self.estado = 8
+
+            elif self.estado == 7:
+                return Simbolo(token=TOKENS['MAI'], lexema=self.__leer_lexema())
+
+            elif self.estado == 8:
+                self.__retrocede_indice()
+                return Simbolo(token=TOKENS['MAY'], lexema=self.__leer_lexema())
+
+            else:
+                if caracter in SIMBOLOS_PERMITIDOS:
+                    return Simbolo(token=ord(caracter), lexema=self.__leer_lexema())
+
+                else:
+                    # TODO generar objeto de error en vez de enviar un mensaje.
+                    self.estado = 0
+                    print("ln: %s. Error Lexico: Simbolo no permitido: '%s'" %
+                        (self.numero_de_linea, self.__leer_lexema())
+                    )
+
+    def __leer_lexema(self):
+        """
+        Regresa la secuencia de caracteres entre el inicio_lexema y el indice,
+        despues mueve el inicio_lexema a la siguiente posicion.
+        """
+        self.lexema = self.codigo[self.inicio_lexema: self.indice + 1]
+        self.__avanza_inicio_lexema()
+        self.estado = 0
+        return self.lexema
+
+    def __retrocede_indice(self):
+        """
+        Retrocede el indice una posicion hacia atras. Equivale al asterisco en
+        los estados de aceptacion.
+        """
+        self.indice -= 1
+
+    def __fallo(self, inicio):
+        """
+        Regresa el valor del estado inicial del siguiente automata a probar
+        cuando el automata anterior fallo.
+        """
+        if inicio == 0:
+            return 9
